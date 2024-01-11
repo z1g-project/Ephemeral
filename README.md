47654c0f4d387e0b8f8d3168ba61fbb29d26f518
incognitotgt <mashoor539@icloud.com>
2024-01-11 00:51:59 -0500

    it works

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Ephermal
 
 This is using Vite and React. To build and start, run `npm run bstart`
+For UI components, we're using `https://ui.shadcn.com/`
+To add a new component run `npx shadcn-ui@latest add [name of component]`
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Input } from "@/components/ui/input"
 export default function Home() {
   const test: string = "hi";
   return (
     <>
       <h1 className="flex items-center p-1 text-blue-600">{test}</h1>
+      <Input />
     </>
   );
 }
