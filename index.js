6b4ae55e6a94a052d1ea29e555e0daf933f3f179
incognitotgt <mashoor539@icloud.com>
2024-01-10 23:16:35 -0500

    routing finally works woohoo

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import { createServer } from "node:http";
 import { createBareServer } from "@tomphttp/bare-server-node";
 import express from "express";
+import path from "path";
 const bare = createBareServer("/bare/");
 const app = express();
 const port = 8080;
 
 app.use(express.static("dist"));
 const server = createServer();
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('dist', 'index.html'));
+});
 
 server.on("request", (req, res) => {
   if (bare.shouldRoute(req)) {
