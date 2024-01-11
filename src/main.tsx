c40eb95ca4bbc082f89e61051915605333450d5e
incognitotgt <mashoor539@icloud.com>
2024-01-10 21:50:26 -0500

    Update package-lock.json and refactor routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Routes from './routes.tsx'
+import Home from './pages/Home'
+import Welcome from './pages/Welcome'
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Routes />
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/welcome" Component={Welcome} />
+      </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 )
