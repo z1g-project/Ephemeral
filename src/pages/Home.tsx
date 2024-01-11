b653e4d4c366a19342b7eae43f15fc6349f625f6
tgt <mashoor539@gmail.com>
2024-01-11 18:03:04 +0000

    Revert "navbar and revert"
    This reverts commit ff1cdf8c6dc1f5cea23eeb2519e9b7fb23735849.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,9 @@
-import { Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle
-} from "@components/ui/navigation-menu";
+import { Input } from "@/components/ui/input"
 export default function Home() {
-  
   const test: string = "hi";
   return (
     <>
       <h1 className="flex items-center p-1 text-blue-600">{test}</h1>
-      <NavigationMenu>
-        <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link to="/settings">
-            <NavigationMenuLink>Settings</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
       <Input />
     </>
   );
