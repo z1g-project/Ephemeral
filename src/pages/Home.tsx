c6b91dff89a8d7a22cec5798509a52b43337cf72
tgt <mashoor539@gmail.com>
2024-01-11 16:39:42 +0000

    navbar and revert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
-import { Input } from "@/components/ui/input"
+import { Link } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle
+} from "@components/ui/navigation-menu";
 export default function Home() {
+  
   const test: string = "hi";
   return (
     <>
       <h1 className="flex items-center p-1 text-blue-600">{test}</h1>
+      <NavigationMenu>
+        <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link to="/settings">
+            <NavigationMenuLink>Settings</NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <Input />
     </>
   );
