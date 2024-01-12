2031a0ec0cc7cac998529c3aea826d8a82a6acf9
incognitotgt <mashoor539@icloud.com>
2024-01-12 00:21:18 -0500

    changed up css vars

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import Routes from "./routes";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="h-screen bg-slate-950">
+    <div className="h-screen bg-slate-950 dark">
       <Routes />
     </div>
   </React.StrictMode>,
