645de2b5ce8f5c38129e856d83c413a2d2031ce7
incognitotgt <mashoor539@icloud.com>
2024-01-13 11:58:08 -0500

    prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,10 +19,12 @@ export default function Home() {
     return encodeURIComponent(
       input
         .toString()
-        .split('')
-        .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt(NaN) ^ 2) : char))
-        .join('')
-    )
+        .split("")
+        .map((char, ind) =>
+          ind % 2 ? String.fromCharCode(char.charCodeAt(NaN) ^ 2) : char,
+        )
+        .join(""),
+    );
   }
   return (
     <>
@@ -48,23 +50,48 @@ export default function Home() {
           </NavigationMenu>
         </div>
         <div className="flex grow items-center justify-center space-x-2">
-          <Input id="input" className="w-80 pr-4" placeholder="Search the web freely" />
-          <Button variant="default" onClick={
-    () => {
-      const input: HTMLInputElement | null = document.getElementById("input") as HTMLInputElement;
-      if (input.value.includes(".") || input.value.includes("https://")) {
-      navigate(`/view?src=${encodeURIComponent(encodeXor(input.value))}`)
-    } else {
-        const src: string = "https://google.com/search?q=" + input.value;
-      navigate(`/view?src=${encodeURIComponent(encodeXor(src))}`)
-    }
-    }}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          <Input
+            id="input"
+            className="w-80 pr-4"
+            placeholder="Search the web freely"
+          />
+          <Button
+            variant="default"
+            onClick={() => {
+              const input: HTMLInputElement | null = document.getElementById(
+                "input",
+              ) as HTMLInputElement;
+              if (
+                input.value.includes(".") ||
+                input.value.includes("https://")
+              ) {
+                navigate(
+                  `/view?src=${encodeURIComponent(encodeXor(input.value))}`,
+                );
+              } else {
+                const src: string =
+                  "https://google.com/search?q=" + input.value;
+                navigate(`/view?src=${encodeURIComponent(encodeXor(src))}`);
+              }
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
             </svg>
           </Button>
         </div>
       </div>
     </>
   );
-    }
+}
