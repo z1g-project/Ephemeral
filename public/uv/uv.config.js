a003a977ed9f0aee19a9bb264128ac682e281688
incognitotgt <mashoor539@icloud.com>
2024-01-13 08:45:44 -0500

    init uv

---
+++ public/uv/uv.config.js
@@ -0,0 +1,12 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: '/light/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.aes.encode,
+    decodeUrl: Ultraviolet.codec.aes.decode,
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
