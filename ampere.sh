5e2830af1213b4bd21fe844352dc9815bf94c6a3
incognitotgt <mashoor539@icloud.com>
2024-01-15 01:02:38 -0500

    comment

--- ampere.sh
+++ ampere.sh
@@ -1,3 +1,6 @@
+<<info
+Comment: This script is used to update Ampere. When you are told to edit src/config.ts, you should change the prefix to "/~/light/" and the server to http://localhost:8080/bare/
+info
 #!/bin/bash
 cd public
 rm -rf ./ampere
