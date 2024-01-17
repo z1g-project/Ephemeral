a6ca2d19fc520dc381de3f78a8936e3b30bce8b3
incognitotgt <mashoor539@icloud.com>
2024-01-16 19:22:41 -0500

    feat: make code 70% faster :open_mouth: (million.js)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import million from "million/compiler";
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
