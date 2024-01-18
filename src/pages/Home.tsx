4efbc533dd5865f63de9636629d2e992b812e345
tgt <mashoor539@gmail.com>
2024-01-18 19:26:19 +0000

    style(home): Make suggestions finally look nice

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,6 @@ import {
 
 import {
   Command,
-  CommandEmpty,
   CommandGroup,
   CommandItem,
   CommandList,
@@ -66,55 +65,64 @@ export default function Home() {
             </NavigationMenuList>
           </NavigationMenu>
         </div>
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg border border-slate-900 shadow-md">
-          <Input
-            id="input"
-            placeholder="Search the web freely"
-            className="z-50 w-96"
-            value={inputValue}
-            onChange={onInputChange}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                if (
-                  inputValue.includes("http://") ||
-                  inputValue.includes("https://")
-                ) {
-                  navigate(`/view/${encodeURIComponent(encoder.encode(inputValue))}`);
-                } else if (
-                  inputValue.includes(".") &&
-                  !inputValue.includes(" ")
-                ) {
-                  navigate(`/view/${encodeURIComponent(encoder.encode("https://" + inputValue))}`);
-                } else {
-                  navigate(
-                    `/view/${encodeURIComponent(encoder.encode(
-                      "https://google.com/search?q=" + inputValue,
-                    ))}`,
-                  );
-                }
+        <Input
+          id="input"
+          placeholder="Search the web freely"
+          className="absolute left-1/2 top-1/2 z-50 w-96 -translate-x-1/2 -translate-y-1/2 !rounded-t-lg focus:!rounded-b-none"
+          value={inputValue}
+          onChange={onInputChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              if (
+                inputValue.includes("http://") ||
+                inputValue.includes("https://")
+              ) {
+                navigate(
+                  `/view/${encodeURIComponent(encoder.encode(inputValue))}`,
+                );
+              } else if (
+                inputValue.includes(".") &&
+                !inputValue.includes(" ")
+              ) {
+                navigate(
+                  `/view/${encodeURIComponent(
+                    encoder.encode("https://" + inputValue),
+                  )}`,
+                );
+              } else {
+                navigate(
+                  `/view/${encodeURIComponent(
+                    encoder.encode("https://google.com/search?q=" + inputValue),
+                  )}`,
+                );
               }
-            }}
-          />
+            }
+          }}
+        />
 
-          <Command>
-            <CommandList>
-              <CommandEmpty>Search for {inputValue}</CommandEmpty>
-              {suggestions.length > 0 && (
-                <CommandGroup heading="Suggestions">
-                  {suggestions.map((suggestion, index) => (
-                    <Link
-                      to={`/view/${encodeURIComponent(encoder.encode(
+        <Command
+          className={`absolute left-1/2 top-[calc(50%_+_10.25rem)] h-auto w-96 -translate-x-1/2 -translate-y-1/2 rounded-b-lg rounded-t-none border-slate-800 shadow-md ${
+            suggestions.length > 0 ? `border-x border-b` : `border-none`
+          }`}
+        >
+          <CommandList>
+            {suggestions.length > 0 && (
+              <CommandGroup heading="Suggestions">
+                {suggestions.map((suggestion, index) => (
+                  <Link
+                    to={`/view/${encodeURIComponent(
+                      encoder.encode(
                         "https://google.com/search?q=" + suggestion,
-                      ))}`}
-                    >
-                      <CommandItem key={index}>{suggestion}</CommandItem>
-                    </Link>
-                  ))}
-                </CommandGroup>
-              )}
-            </CommandList>
-          </Command>
-        </div>
+                      ),
+                    )}`}
+                  >
+                    <CommandItem key={index}>{suggestion}</CommandItem>
+                  </Link>
+                ))}
+              </CommandGroup>
+            )}
+          </CommandList>
+        </Command>
       </div>
     </>
   );
--- src/utils/encoder.ts
+++ src/utils/encoder.ts
@@ -4,14 +4,13 @@ const aesKey = window.location.origin + navigator.userAgent;
 
 export const encoder = {
   encode: (str: string) => {
-      if (!str) return str;
+    if (!str) return str;
 
-      return AES.encrypt(str, aesKey).toString()
-      .substring(10);
+    return AES.encrypt(str, aesKey).toString().substring(10);
   },
   decode: (str: string) => {
-      if (!str) return str;
+    if (!str) return str;
 
-      return AES.decrypt('U2FsdGVkX1' + str, aesKey).toString(Utf8);
+    return AES.decrypt("U2FsdGVkX1" + str, aesKey).toString(Utf8);
   },
 };
