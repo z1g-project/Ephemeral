4afc31bc50fa657e34c85905019f21db1a7b79ed
tgt <mashoor539@gmail.com>
2024-01-18 17:04:25 +0000

    feat: add AES support for UV fix: remove package-lock.json

---
+++ src/utils/encoder.ts
@@ -0,0 +1,17 @@
+import AES from "crypto-js/aes";
+import Utf8 from "crypto-js/enc-utf8";
+const aesKey = window.location.origin + navigator.userAgent;
+
+export const encoder = {
+  encode: (str: string) => {
+      if (!str) return str;
+
+      return AES.encrypt(str, aesKey).toString()
+      .substring(10);
+  },
+  decode: (str: string) => {
+      if (!str) return str;
+
+      return AES.decrypt('U2FsdGVkX1' + str, aesKey).toString(Utf8);
+  },
+};
