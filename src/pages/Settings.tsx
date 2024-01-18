86d4f0b13f5d7b20ff79c0f3cdadfb96c026adfc
incognitotgt <mashoor539@icloud.com>
2024-01-18 18:36:00 -0500

    fix(view): search bar not encoding chore(settings): init

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+export default function Settings() {
+  return (
+    <div className="flex min-h-screen bg-slate-950">
+      <div className="absolute right-1 -translate-y-2 justify-end p-5">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link to="/">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/settings">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Settings
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/apps">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Apps
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
+  );
+}
