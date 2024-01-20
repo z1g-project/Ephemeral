bdc08bcb675eb867b07ba4f757faf9b4ecaf0014
Violet <vbnm0149@gmail.com>
2024-01-20 12:58:33 -0500

    slight update

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -7,24 +7,47 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 export default function Settings() {
   return (
     <div className="flex min-h-screen bg-slate-950">
       <Navbar />
-      <div className="absolute left-1/2 -translate-x-1/2 font-['Inter'] text-3xl font-extrabold text-slate-400">
+      <div className="absolute left-1/2 top-5 -translate-x-1/2 font-['Inter'] text-3xl font-extrabold text-slate-400">
         Settings
       </div>
       <div className="h-72 w-96 p-6"></div>
-      <Card>
+      <Card className="absolute w-[500px] bg-slate-950 left-20 top-20">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle>Accessibility</CardTitle>
+          <CardDescription>placeholder</CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
+        <CardContent className="bg-slate-950">
+          <Label htmlFor="name">Search Engine</Label>
+          <Select>
+            <SelectTrigger id="search" className="bg-slate-950">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent position="popper" className="bg-slate-950 text-white">
+              <SelectItem value="google">Google</SelectItem>
+              <SelectItem value="yahoo">Yahoo</SelectItem>
+              <SelectItem value="bing">Bing</SelectItem>
+              <SelectItem value="duckduckgo">DuckDuckGo</SelectItem>
+            </SelectContent>
+          </Select>
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
+        <CardFooter className="flex justify-between">
+          <Button variant="destructive">Reset Changes</Button>
+          <Button variant="default">Save Changes</Button>
         </CardFooter>
       </Card>
     </div>
