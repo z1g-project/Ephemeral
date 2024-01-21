c435540ce9d9edd1f2b0c254c6e35dd1c8896016
incognitotgt <mashoor539@icloud.com>
2024-01-20 19:26:05 -0500

    fix: make search suggestions change pointer feat: add serviceworker and 404 pages chore: start to options to settings (not working yet) chore(navbar): add title (at last)

---
+++ src/pages/ServiceWorkerError.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+export default function PageNotFound() {
+  return (
+    <div className="flex min-h-screen bg-slate-950">
+      <div className="absolute left-1/2 top-[calc(50%_-_10.25rem)] -translate-x-1/2 -translate-y-1/2 font-['Inter'] text-3xl font-extrabold text-slate-400">
+        Service Worker Error. Click below to register the service worker.
+      </div>
+      <Button
+        type="button"
+        onClick={() => window.location.reload()}
+        className="absolute left-1/2 top-[calc(50%_+_10.25rem)] -translate-x-1/2 -translate-y-1/2 font-['Inter'] text-slate-800"
+      >
+        Refresh
+      </Button>
+    </div>
+  );
+}
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,14 +1,15 @@
+import { useRef } from "react";
 import Navbar from "@/components/Navbar";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
+  // CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -16,40 +17,64 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 
 export default function Settings() {
+  const proxyDropdownRef = useRef(null);
+  function toUpperCase(str: string): string {
+    return str?.charAt(0).toUpperCase() + str?.slice(1);
+  }
   return (
     <div className="flex min-h-screen bg-slate-950">
       <Navbar />
-      <div className="absolute left-1/2 top-5 -translate-x-1/2 font-['Inter'] text-3xl font-extrabold text-slate-400">
+      <div className="absolute left-1/2 top-5 -translate-x-1/2 translate-y-5 font-['Inter'] text-3xl font-extrabold text-slate-400">
         Settings
       </div>
-      <div className="h-72 w-96 p-6"></div>
-      <Card className="absolute w-[500px] bg-slate-950 left-20 top-20">
-        <CardHeader>
-          <CardTitle>Accessibility</CardTitle>
-          <CardDescription>placeholder</CardDescription>
-        </CardHeader>
-        <CardContent className="bg-slate-950">
-          <Label htmlFor="name">Search Engine</Label>
-          <Select>
-            <SelectTrigger id="search" className="bg-slate-950">
-              <SelectValue placeholder="Select" />
-            </SelectTrigger>
-            <SelectContent position="popper" className="bg-slate-950 text-white">
-              <SelectItem value="google">Google</SelectItem>
-              <SelectItem value="yahoo">Yahoo</SelectItem>
-              <SelectItem value="bing">Bing</SelectItem>
-              <SelectItem value="duckduckgo">DuckDuckGo</SelectItem>
-            </SelectContent>
-          </Select>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="destructive">Reset Changes</Button>
-          <Button variant="default">Save Changes</Button>
-        </CardFooter>
-      </Card>
+      <div className="absolute space-x-5 p-6 ">
+        <Card className="absolute left-20 top-20 h-96 w-96">
+          <CardHeader>
+            <CardTitle>Proxy</CardTitle>
+            <CardDescription>Set proxy settings</CardDescription>
+          </CardHeader>
+          <CardContent className="bg-slate-950">
+            <Label htmlFor="name">Backend</Label>
+            <Select>
+              <SelectTrigger className="bg-slate-950">
+                <SelectValue
+                  ref={proxyDropdownRef}
+                  placeholder={toUpperCase(
+                    localStorage.getItem("proxy") || "ultraviolet",
+                  )}
+                />
+              </SelectTrigger>
+              <SelectContent
+                position="popper"
+                className="bg-slate-950 text-white"
+                onChange={(e) => {
+                  // not working
+                  localStorage.setItem(
+                    "proxy",
+                    (e.target as HTMLInputElement).value,
+                  );
+                }}
+              >
+                <SelectItem value="ultraviolet">Ultraviolet</SelectItem>
+                <SelectItem value="ampere">Ampere</SelectItem>
+              </SelectContent>
+            </Select>
+            <Label htmlFor="name">Bare Server</Label>
+            <Input
+              id="name"
+              placeholder="Type a valid Bare URL"
+              className="bg-slate-950"
+              defaultValue={localStorage.getItem("bareServer") || ""}
+              onChange={(e) => {
+                localStorage.setItem("bareServer", e.target.value);
+              }}
+              />
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
