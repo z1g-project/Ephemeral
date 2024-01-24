517c32d6094117785576bc20799ccae9e5820262
incognitotgt <mashoor539@icloud.com>
2024-01-24 12:09:44 -0500

    chore: settings card for cloaking

--- src/pages/settings/CloakSettings.tsx
+++ src/pages/settings/CloakSettings.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import {
   Card,
   CardContent,
@@ -7,12 +6,55 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-export default function CloakSettings(props: cardProps) {
-    return (
-        <>
-        <Card className={cn("h-96 w-96",props.className)}>
-            
-        </Card>
-        </>
-    )
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+export default function CloakSettings() {
+  return (
+    <>
+      <Card className="h-96 w-96">
+        <CardHeader>
+          <CardTitle>Cloak</CardTitle>
+          <CardDescription>Set cloaking settings</CardDescription>
+        </CardHeader>
+        <CardContent className="bg-slate-950">
+          <Label htmlFor="presets">Presets</Label>
+          <Select>
+            <SelectTrigger className="bg-slate-950">
+              <SelectValue placeholder="Select a preset" />
+              <SelectContent position="popper" className="bg-slate-950">
+                <SelectItem value="schoology">Schoology</SelectItem>
+                <SelectItem value="gclassroom">Google Classroom</SelectItem>
+                <SelectItem value="canvas">Canvas</SelectItem>
+              </SelectContent>
+            </SelectTrigger>
+          </Select>
+          <Label htmlFor="title">Page Title</Label>
+          <Input
+            id="page-title"
+            type="text"
+            placeholder="Set how the tab title looks"
+            className="bg-slate-950"
+          />
+          <Label htmlFor="title">Page Favicon</Label>
+          <Input
+            id="page-favicon"
+            type="text"
+            placeholder="Set the favicon"
+            className="bg-slate-950"
+          />
+        </CardContent>
+        <CardFooter>
+          <Button variant="default">Save</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
