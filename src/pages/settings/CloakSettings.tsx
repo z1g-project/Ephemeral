cc33b575855e4e8f25051e4ad75e8f2ac1ba05ee
incognitotgt <mashoor539@icloud.com>
2024-01-24 11:48:18 -0500

    refactor: make every settings card a component

---
+++ src/pages/settings/CloakSettings.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+export default function CloakSettings(props: cardProps) {
+    return (
+        <>
+        <Card className={cn("h-96 w-96",props.className)}>
+            
+        </Card>
+        </>
+    )
+}
