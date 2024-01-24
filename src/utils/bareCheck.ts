12f4d857516790a9c68aa4afa51a2f3f32e751d1
incognitotgt <mashoor539@icloud.com>
2024-01-23 23:53:10 -0500

    fix: bareswitcher

--- src/utils/bareCheck.ts
+++ src/utils/bareCheck.ts
@@ -1,31 +1,3 @@
-export async function bareVerify(bare: string): Promise<boolean> {
-  const headers = new Headers({
-    "x-bare-url": "https://www.google.com",
-    "X-Bare-Headers": JSON.stringify({
-      Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    }),
-  });
-  fetch(bare + "v3/", {
-    method: "GET",
-    headers: headers,
-  })
-    .then((res) => {
-      if (
-        res.headers.get("x-bare-status") === "200" ||
-        res.headers.get("x-bare-status") === "302"
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    })
-    .catch((error) => {
-    console.log(error)
-      return false;
-    });
-    return false;
-}
 export async function proxyCompat(bare: string): Promise<boolean> {
   const headers = new Headers({
     "x-bare-url": "https://www.google.com",
@@ -34,7 +6,7 @@ export async function proxyCompat(bare: string): Promise<boolean> {
         "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     }),
   });
-  fetch(bare + "v3/", {
+  await fetch(bare + "v3/", {
     method: "GET",
     headers: headers,
   })
