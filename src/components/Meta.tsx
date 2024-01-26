8e4887dd4f05cf90d9da6e92e78f5a168271b953
incognitotgt <mashoor539@icloud.com>
2024-01-26 10:54:20 -0500

    chore: add about dialog chore(style): better settings page design chore: version is 0.2.0 todo: Make settings page UI tabbed.

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -74,7 +74,7 @@ export default function Meta() {
             }]
            }
             `}
-        </script>
+      </script>
     </Helmet>
   );
 }
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -4,6 +4,16 @@ import SearchSettings from "./settings/SearchSettings";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+//import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function Settings() {
   return (
@@ -15,10 +25,62 @@ export default function Settings() {
           Settings
         </div>
         <Toaster />
-        <div className="top-1/2 m-5 mt-20 flex space-x-4">
+        // todo: make this a tab component
+        <div className="flex flex-grow flex-row items-center justify-center space-x-4">
           <ProxySettings />
           <CloakSettings />
           <SearchSettings />
+        </div>
+        <div className="absolute bottom-5 left-5 text-slate-200">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="link">About Ephermal</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="text-slate-200">
+                  About Ephermal 0.2.0 - Beta
+                </DialogTitle>
+                <DialogDescription>
+                  Ephermal is a powerful proxy with Ultraviolet and other
+                  features, made by the z1g Project.
+                  <br />
+                  Ephermal is made possble by the following projects:
+                  <br />
+                  <li>Ultraviolet</li>
+                  <li>Ampere</li>
+                  <li>React</li>
+                  <li>Vite</li>
+                  Developers:
+                  <br />
+                  <li>
+                    <Button asChild variant="link">
+                      <a href="https://github.com/notplayingallday383">
+                        Playingallday383
+                      </a>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button asChild variant="link">
+                      <a href="https://github.com/incognitotgt">tgt</a>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button asChild variant="link">
+                      <a href="https://github.com/interpolation-0">
+                        interpolation
+                      </a>
+                    </Button>
+                  </li>
+                  <li>
+                    <Button asChild variant="link">
+                      <a href="https://github.com/vbnm0">vy.x</a>
+                    </Button>
+                  </li>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </>
