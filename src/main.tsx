268983996c1e378fc1814a76a3c7b6a259d7b19f
incognitotgt <mashoor539@icloud.com>
2024-01-25 19:24:33 -0500

    chore: add metadata

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Routes from "./routes";
 import "./index.css";
+import Meta from "./components/Meta";
 declare global {
   interface Window {
     __uv$config: any;
@@ -11,6 +12,7 @@ declare global {
 }
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Meta />
     <Routes />
   </React.StrictMode>,
 );
