8e96e92e4763280f81d79b35258089b19295703f
incognitotgt <mashoor539@icloud.com>
2024-02-12 19:58:35 -0500

    chore: removed axios

--- src/pages/settings/SearchSettings.tsx
+++ src/pages/settings/SearchSettings.tsx
@@ -26,88 +26,80 @@ export default function SearchSettings() {
   const customSearchRef = useRef<HTMLInputElement>(null);
   const selectRef = useRef<HTMLSelectElement>(null);
   return (
-    <>
-      <Card className="h-96 w-96">
-        <CardHeader>
-          <CardTitle>Search</CardTitle>
-          <CardDescription>Set search engine</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Label htmlFor="presets" className="text-sm font-medium">
-            Presets
-          </Label>
-          <Select
-            aria-label="Search Engine"
-            onValueChange={(value) => {
-              localStorage.setItem("search", value);
-              if (value === "Google") {
-                localStorage.setItem(
-                  "searchUrl",
-                  "https://google.com/search?q=",
-                );
-              } else if (value === "DuckDuckGo") {
-                localStorage.setItem("searchUrl", "https://duckduckgo.com/?q=");
-              } else if (value === "Bing") {
-                localStorage.setItem("searchUrl", "https://bing.com/search?q=");
+    <Card className="h-96 w-96">
+      <CardHeader>
+        <CardTitle>Search</CardTitle>
+        <CardDescription>Set search engine</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Label htmlFor="presets" className="text-sm font-medium">
+          Presets
+        </Label>
+        <Select
+          aria-label="Search Engine"
+          onValueChange={(value) => {
+            localStorage.setItem("search", value);
+            if (value === "Google") {
+              localStorage.setItem("searchUrl", "https://google.com/search?q=");
+            } else if (value === "DuckDuckGo") {
+              localStorage.setItem("searchUrl", "https://duckduckgo.com/?q=");
+            } else if (value === "Bing") {
+              localStorage.setItem("searchUrl", "https://bing.com/search?q=");
+            }
+            setSearchUrl(localStorage.getItem("searchUrl") || "");
+            toast({
+              title: "Search Engine Changed",
+              description: "Search Engine has been changed to " + value,
+            });
+          }}
+        >
+          <SelectTrigger aria-label="Presets">
+            <SelectValue
+              placeholder={
+                localStorage.getItem("search") || "Select a search engine"
               }
-              setSearchUrl(localStorage.getItem("searchUrl") || "");
-              toast({
-                title: "Search Engine Changed",
-                description: "Search Engine has been changed to " + value,
-              });
-            }}
-          >
-            <SelectTrigger aria-label="Presets">
-              <SelectValue
-                placeholder={
-                  localStorage.getItem("search") || "Select a search engine"
-                }
-                ref={selectRef}
-              />
-            </SelectTrigger>
-            <SelectContent position="popper" className=" text-white">
-              <SelectItem value="Google">Google</SelectItem>
-              <SelectItem value="DuckDuckGo">DuckDuckGo</SelectItem>
-              <SelectItem value="Bing">Bing</SelectItem>
-            </SelectContent>
-          </Select>
-          <Label htmlFor="search-url" className="text-sm font-medium">
-            Search Engine URL
-          </Label>
-          <Input
-            id="search-url"
-            ref={customSearchRef}
-            spellCheck={false}
-            placeholder="Enter a valid Search Engine URL"
-            value={searchUrl}
-            onChange={(e) => setSearchUrl(e.target.value)}
-          />
-        </CardContent>
-        <CardFooter>
-          <Button
-            onClick={() => {
-              localStorage.setItem("search", "Custom");
-              selectRef.current!.value = "Custom";
-              if (customSearchRef.current!.value === "") {
-                localStorage.setItem(
-                  "searchUrl",
-                  "https://google.com/search?q=",
-                );
-                localStorage.setItem("search", "Google");
-              }
-              localStorage.setItem("searchUrl", customSearchRef.current!.value);
-              toast({
-                title: "Search Engine Changed",
-                description:
-                  "Search Engine has been changed to " +
-                  customSearchRef.current!.value,
-              });
-            }}
-          >
-            Save
-          </Button>
-        </CardFooter>
-      </Card>
-    </>
+              ref={selectRef}
+            />
+          </SelectTrigger>
+          <SelectContent position="popper" className=" text-white">
+            <SelectItem value="Google">Google</SelectItem>
+            <SelectItem value="DuckDuckGo">DuckDuckGo</SelectItem>
+            <SelectItem value="Bing">Bing</SelectItem>
+          </SelectContent>
+        </Select>
+        <Label htmlFor="search-url" className="text-sm font-medium">
+          Search Engine URL
+        </Label>
+        <Input
+          id="search-url"
+          ref={customSearchRef}
+          spellCheck={false}
+          placeholder="Enter a valid Search Engine URL"
+          value={searchUrl}
+          onChange={(e) => setSearchUrl(e.target.value)}
+        />
+      </CardContent>
+      <CardFooter>
+        <Button
+          onClick={() => {
+            localStorage.setItem("search", "Custom");
+            selectRef.current!.value = "Custom";
+            if (customSearchRef.current!.value === "") {
+              localStorage.setItem("searchUrl", "https://google.com/search?q=");
+              localStorage.setItem("search", "Google");
+            }
+            localStorage.setItem("searchUrl", customSearchRef.current!.value);
+            toast({
+              title: "Search Engine Changed",
+              description:
+                "Search Engine has been changed to " +
+                customSearchRef.current!.value,
+            });
+          }}
+        >
+          Save
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
