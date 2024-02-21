f72f19d11adb4a7b01f27fb29275da39ef6e2631
Violet <vbnm0149@gmail.com>
2024-02-21 17:25:24 -0500

    fix: always 5 columns for any screen size kinda breaks

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -18,7 +18,7 @@ export default function Apps() {
 				<span className="inline-block w-full text-center text-3xl font-bold text-slate-300">
 					Apps
 				</span>
-				<div className="grid grid-cols-5 gap-4 p-8">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6 4xl:grid-cols-7 gap-4 p-8">
 					{apps.map((app) => (
 						<div className="w-64">
 							<a
