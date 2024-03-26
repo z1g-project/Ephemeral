847a3ef733301898c6373063ecefbe9485f6a2cb
Anshnk <73756572+anshnk@users.noreply.github.com>
2024-03-26 15:28:32 +0000

    Added Tooltip above Themes button.

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -8,23 +8,40 @@ import {
 	DropdownMenuLabel,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger
+} from "@/components/ui/tooltip"
+
 import { useTheme, themes } from "@/components/theme-provider";
 
 export function ModeToggle({ text }: { text?: string }) {
 	const { setTheme } = useTheme();
 	return (
+
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size={text ? "default" : "icon"}>
-					{text}
-					<SwatchBook
-						className={`${text && "ml-2"} h-[1.2rem] w-[1.2rem] transition-all`}
-					/>
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Button variant="outline" size={text ? "default" : "icon"}>
+						{text}
+						<SwatchBook
+							className={`${text && "ml-2"} h-[1.2rem] w-[1.2rem] transition-all`}
+						/>
+						<span className="sr-only">Toggle theme</span>
+					</Button>
+						</TooltipTrigger>
+						<TooltipContent>Themes</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+		</DropdownMenuTrigger>
+			
 			<DropdownMenuContent align="end">
-				<DropdownMenuLabel>Theme</DropdownMenuLabel>
+				<DropdownMenuLabel>Themes</DropdownMenuLabel>
 				{themes.map((theme) => (
 					<DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
 						{theme.charAt(0).toUpperCase() + theme.slice(1)}
@@ -35,5 +52,6 @@ export function ModeToggle({ text }: { text?: string }) {
 				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
-	);
+
+);
 }
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
+
 type Theme =
 	| "dark"
 	| "light"
