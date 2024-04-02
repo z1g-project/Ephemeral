c7dbd1019614d78150b5a9d5f351ae5abdb8dbc5
incognitotgt <tgt@incognitotgt.me>
2024-04-02 13:08:28 -0400

    add static hosting instructions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 
 declare const __BUILD_DATE__: Date;
+interface ImportMetaEnv {
+	readonly VITE_WISP_SERVER: string;
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
